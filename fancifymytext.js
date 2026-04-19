239aeae7a965a2e507f2b7eb31989abfd333e3ff
Yeremi Navarrete <yereminavarrete03@gmail.com>
2026-04-18 22:35:06 -0700

    Updated to include the last assignment requirement with new changes to the handler, FanyShmancy, BoringBetting, and included a new button the Moo buton.

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,7 +1,29 @@
 function AlertButton() {
     alert("Hello, world!");
+    document.getElementById("inputText").style.fontSize = "24pt";
+    document.getElementById("inputText").style.background = "red";
 }
 
-var button = document.getElementById("Bigger");
+function AlertButton2() {
+    alert("Changing styles eh?");
+    document.getElementById("inputText").style.fontWeight = "bold";
+    document.getElementById("inputText").style.color = "blue";
+    document.getElementById().style.textDecoration = "underline";
+}
 
-button.addEventListener("click", AlertButton);
+function AlertButton3() {
+    alert("why did you change back....that so boring");
+    document.getElementById("inputText").style.fontWeight = "normal";
+    document.getElementById("inputText").style.fontSize = "10pt";
+    document.getElementById("inputText").style.background = "white";
+    document.getElementById("inputText").style.color = "black";
+    document.getElementById("inputText").style.textDecoration = "none";
+}
+
+function modifyingText(){
+    var text = document.getElementById("inputText").value;
+    text = text.toUpperCase();
+    var delimiter = text.split(" ");
+    text = delimiter.join("-Moo ");
+    document.getElementById("inputText").value = text;
+}
